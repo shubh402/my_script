18e29e064a7e78c44277544e69f16d95a068803f
Shubh <vishwajitg471@gmail.com>
2022-06-09 11:59:00 +0530

    add my 'neovim' config file
    Change-Id: I67c55075f1d09e15bb6ad3ef0762f198ce071843
    Signed-off-by: Shubh <vishwajitg471@gmail.com>
    
    i now use fish shell btw
    
    Change-Id: If8fdf558a85e82ca5b7d563dd6d2d1e2ac791764
    Signed-off-by: Shubh <vishwajitg471@gmail.com>
    
    update latest changes and now install oh-my-fish
    
    Change-Id: I42ac6a2dc94950d3bae8e66db24673cb41cfd00c
    Signed-off-by: Shubh <vishwajitg471@gmail.com>
    
    setup vim.plug
    
    Change-Id: I87782c613ba062a227a74c13084e243f0de41557
    Signed-off-by: Shubh Gupta <vishwajitg471@gmail.com>
    
    format and remove extra spaces
    
    Change-Id: I0235259d47da14f00d09a54e7db4caa7e0d0aa44
    Signed-off-by: Shubh Gupta <vishwajitg471@gmail.com>

--- mac_setup.sh
+++ mac_setup.sh
@@ -2,7 +2,7 @@
 brew update && brew upgrade
 
 # install some pakages
-brew install git gcc node docker
+brew install git gcc node docker fish
 
 # setup my git configs
 git config --global user.name "Shubh"
@@ -13,3 +13,10 @@ git config --global init.templatedir '~/.git-templates'
 mkdir -p ~/.git-templates/hooks
 curl -Lo ~/.git-templates/hooks/commit-msg https://raw.githubusercontent.com/Imshubh69/my_script/main/commit-msg
 chmod 755 ~/.git-templates/hooks/commit-msg
+
+# setup oh my fish too
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# setup vim.plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
