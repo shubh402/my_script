cb4fd26913f4ad51b0f068068492bb2f2720d7b1
Shubh <vishwajitg471@gmail.com>
2022-06-25 21:22:05 +0530

    added my yarn update script
    Change-Id: Ia5e1f9416ddd89f86b6d64381601f3f4d3271f23
    Signed-off-by: Shubh <vishwajitg471@gmail.com>

--- mac_setup.sh
+++ mac_setup.sh
@@ -23,3 +23,16 @@ curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install 
 # setup vim.plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+
+function yarn-s () {
+                     yarn set version stable
+                     echo '.yarn/*
+                     !.yarn/cache
+                     !.yarn/patches
+                     !.yarn/plugins
+                     !.yarn/releases
+                     !.yarn/sdks
+                     !.yarn/versions'>> .gitignore
+                     }
